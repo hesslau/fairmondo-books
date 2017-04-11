44d6f4242ff7a2e79e1b9b0f39e3abfbc69b202c
Henrik Hesslau <henrik@hesslau.org>
2017-04-11 14:57:02 +0200

    Add Export Model

---
+++ database/migrations/2017_04_11_121942_create_export_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exports', function(Blueprint $table) {
+            $table->integer('id');
+            $table->primary('id');
+            $table->integer('number_of_products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exports');
+    }
+}
