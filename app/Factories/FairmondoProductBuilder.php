7a91f419e4fb2d132011468a66199d390d49bf79
Henrik Hesslau <henrik@hesslau.org>
2017-04-12 15:35:48 +0200

    testing

--- app/Factories/FairmondoProductBuilder.php
+++ app/Factories/FairmondoProductBuilder.php
@@ -35,13 +35,13 @@ class FairmondoProductBuilder {
             }
 
             $product->title = self::getTitle($source);
-            $product->categories = join(',',self::getCategories($source));
+            //$product->categories = join(',',self::getCategories($source));
             $product->quantity = self::getQuantity($source);
-            $product->content = self::getContent($source);
+            //$product->content = self::getContent($source);
             $product->vat = self::getVat($source);
             $product->external_title_image_url = self::getExternalTitleImageUrl($source);
             $product->gtin = self::getGtin($source);
-            $product->action = self::getAction($source);
+            //$product->action = self::getAction($source);
             $product->price_cents = self::getPriceCents($source);
             $product->transport_time = self::getTransportTime($source);
             $product->custom_seller_identifier = self::getCustomSellerIdentifier($source);
