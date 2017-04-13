ac915b0855ef8c3b9479a89b5cf5a5d8cb5d01cf
Henrik Hesslau <henrik@hesslau.org>
2017-04-14 00:14:12 +0200

    optimize

--- app/Models/LibriProduct.php
+++ app/Models/LibriProduct.php
@@ -21,14 +21,15 @@ class LibriProduct extends Model
     }
 
     public function isAudioBook() {
-        $productGroup = (Integer) substr($this->attributes['VLBSchemeOld'],0,2);
+        if(!key_exists('VLBSchemeOld',$this->attributes)) return false;
+        $productGroup = $this->attributes['VLBSchemeOld'];
 
         /*  Die Warengruppe, die Musik-CDs umfassend abgebildet ist die 59*.
 	        Die Warengruppen 51 bis 58 bezeichnen Hörbücher zu den jeweiligen Themen aus der Warengruppensystematik.
             Da die 559 die Warengruppe „Musik“ (CDs) bezeichnet, können auch da Musik-CDs reingeraten.  */
 
         // if ProductForm is AC (Audio-CD), DB (CD), DA (MP3-CD) or AI (Audio-DVD) and ProductGroup is non-music it's an audiobook.
-        if(in_array($this->attributes['ProductForm'],['AC','DB','DA','AI']) and $productGroup >= 51 and $productGroup <= 58) {
+        if(in_array($this->attributes['ProductForm'],['AC','DB','DA','AI']) and $productGroup >= 5100 and $productGroup <= 5899) {
             return true;
         } else {
             return false;
