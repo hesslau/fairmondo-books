7296ed488be6b5195b3c668b08e918729585cc3b
Henrik Hesslau <henrik@hesslau.org>
2017-04-13 16:25:30 +0200

    add todo

--- app/Models/LibriProduct.php
+++ app/Models/LibriProduct.php
@@ -14,6 +14,7 @@ class LibriProduct extends Model
         return "$value";
     }
 
+    // todo outsource raw data to config file
     public function isDigital() {
         $digitalProductForms = ['AC','DB','DA','AI','VI','VO','ZE','00'];
         return in_array($this->attributes['ProductForm'],$digitalProductForms);
