59d604a5ab70e8eec8294c329669c14ffbbc0a73
Henrik Hesslau <henrik@hesslau.org>
2017-04-13 16:01:32 +0200

    correct test

--- tests/ExportControllerTest.php
+++ tests/ExportControllerTest.php
@@ -10,19 +10,13 @@ class ExportControllerTest extends TestCase
 {
     public function testExport()
     {
-        // filepath to export file
-        $target = storage_path('app/export/TEXT.csv');
-
-        // delete if exported file already exists
-        if(file_exists($target)) unlink($target);
-
-        // create sample data in database
-        FairmondoProductTest::createFairmondoProduct()->save();
-
-        // export all data in database
-        ExportController::exportFairmondoProducts($target);
+        // test export
+        $exportFile = ExportController::makeDelta(null, true);
 
         // asser that export file has been written
-        $this->assertTrue(file_exists($target));
+        $this->assertTrue(file_exists($exportFile));
+
+        // delete file
+        unlink($exportFile);
     }
 }
