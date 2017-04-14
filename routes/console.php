9dbd35e149c5d711892d51acbae2da6804b8b79a
Henrik Hesslau <henrik@hesslau.org>
2017-04-14 09:54:00 +0200

    add initial import routine

--- routes/console.php
+++ routes/console.php
@@ -44,3 +44,22 @@ Artisan::command('pull:annotations', function(){
 Artisan::command('fairmondobooks:export {--since} {--test}', function($since, $test) {
     App\Http\Controllers\ExportController::makeDelta($since, $test);
 });
+
+Artisan::command('fairmondobooks:initialImport', function() {
+    function c($gtin) {
+        $libriProduct = App\Factories\LibriProductFactory::makeFakeProduct(['ProductReference'=>$gtin]);
+        $farProduct = App\Factories\FairmondoProductBuilder::create($libriProduct);
+        if(is_null($farProduct)) Illuminate\Support\Facades\Log::error("Could not create FairmondoProduct for $gtin.");
+        else $farProduct->save();
+    }
+
+    $handle = fopen("../books-export-henrik.csv", "r");
+    ob_start();
+    while(($gtin=fgets($handle)) !== false) {
+        c($gtin);
+    }
+    ob_end_clean();
+    fclose($handle);
+    echo "All Done!";
+    Illuminate\Support\Facades\Log::info("Done with initial import!");
+});
