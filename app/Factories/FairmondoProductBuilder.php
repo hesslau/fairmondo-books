0accb71bd395e2c5b748ba8f6caace7fc60aaa3c
Henrik Hesslau <henrik@hesslau.org>
2017-04-18 08:38:56 +0200

    add comment

--- app/Factories/FairmondoProductBuilder.php
+++ app/Factories/FairmondoProductBuilder.php
@@ -74,6 +74,7 @@ class FairmondoProductBuilder {
         $conditions = array(
             "HasDistinctiveTitle"       => ($product->DistinctiveTitle != ''),     // note: useless condition since products without title will fail the previous import
             "HasValidProductForm"       => in_array($product->ProductForm,$validProductForms),
+            // todo What happens when AvailabilityStatus changes?
             "IsAvailable"               => $product->AvailabilityStatus == $validAvailabilityStatus,
             "HasAppropriateAudience"    => (isset($product->AudienceCodeValue) and !in_array($product->AudienceCodeValue,$invalidAudienceCodeValues)),
             //"HasQuantityOnHand"         => ($product->QuantityOnHand > 0) // or $product->Lib_MSNo = 15) // @todo what is Lib_MSNo???
