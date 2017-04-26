8ae7cf89803c3998292fd7e1bb2511b4aa661290
Henrik Hesslau <henrik@hesslau.org>
2017-04-26 09:12:59 +0200

    add Log Facade to ExportController

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -6,6 +6,7 @@ use App\Facades\ConsoleOutput;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use League\Csv\Writer;
 use App\Models\FairmondoProduct;
 use App\Models\LibriProduct;
