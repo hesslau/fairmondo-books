2531d4e15039fe7daa75da8a2e5f375d2697d83c
Henrik Hesslau <henrik@hesslau.org>
2017-05-17 12:49:30 +0200

    add migration to change libri_products.PriceAmount column to float and add 1 to each price

---
+++ database/migrations/2017_05_17_123252_change_PriceAmount_column_to_float.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class ChangePriceAmountColumnToFloat extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+{
+    DB::statement('ALTER TABLE `libri_products` MODIFY `PriceAmount` FLOAT UNSIGNED NOT NULL;');
+    DB::statement('UPDATE libri_products SET PriceAmount = PriceAmount+1');
+}
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+{
+    DB::statement('UPDATE libri_products SET PriceAmount = PriceAmount-1');
+    DB::statement('ALTER TABLE `libri_products` MODIFY `PriceAmount` INTEGER UNSIGNED NULL;');
+}
+}
