ce7eb2308c6bf5f01bf245782f9666aa5ef54bd1
Henrik Hesslau <henrik@hesslau.org>
2017-05-18 10:52:43 +0200

    add ImportManager

---
+++ app/Managers/ImportManager.php
@@ -0,0 +1,34 @@
+***REMOVED***
+/**
+ * Created by PhpStorm.
+ * User: hesslau
+ * Date: 5/18/17
+ * Time: 10:27 AM
+ */
+
+namespace App\Managers;
+
+
+use App\Facades\ConsoleOutput;
+use App\Factories\IFactory;
+
+/*
+ * Imports files into Database.
+ */
+class ImportManager
+{
+    public function __construct(IFactory $_factory) {
+        $this->factory = $_factory;
+    }
+
+    /*
+     * Imports a file into database by calling the factory functions.
+     */
+    public function importFile($filename) {
+        ConsoleOutput::info("Parse ${filename}:");
+        $items = $this->factory->makeFromFile($filename);
+
+        ConsoleOutput::info("Store ${filename}:");
+        $this->factory->store($items);
+    }
+}
