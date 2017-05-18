e52e75cf3387b017461fb11c2bb3f8d174ba5b00
Henrik Hesslau <henrik@hesslau.org>
2017-05-18 11:15:48 +0200

    add migration to make column libri_products.CatalogUpdate nullable

---
+++ database/migrations/2017_05_18_110714_make_column_CatalogUpdate_nullable.php
@@ -0,0 +1,28 @@
+***REMOVED***
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeColumnCatalogUpdateNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE `libri_products` MODIFY `CatalogUpdate` INTEGER UNSIGNED NULL;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('ALTER TABLE `libri_products` MODIFY `CatalogUpdate` INTEGER UNSIGNED NOT NULL;');
+    }
+}
