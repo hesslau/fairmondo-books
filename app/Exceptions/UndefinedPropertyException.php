433f42ca2578b391bb58f7a4e81eb19515f935dd
Henrik Hesslau <henrik@hesslau.org>
2017-05-19 09:35:18 +0200

    throw UndefinedPropertyException when accessing a property that doesn't exist

---
+++ app/Exceptions/UndefinedPropertyException.php
@@ -0,0 +1,15 @@
+***REMOVED***
+/**
+ * Created by PhpStorm.
+ * User: hesslau
+ * Date: 2/27/17
+ * Time: 4:19 PM
+ */
+
+namespace App\Exceptions;
+use Exception;
+
+class UndefinedPropertyException extends Exception
+{
+
+}
