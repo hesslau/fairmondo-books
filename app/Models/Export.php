5878f8534339360c61edc2aa236cab4d3d5a90b5
Henrik Hesslau <henrik@hesslau.org>
2017-10-18 09:33:46 +0200

    refactor code & implement latest export socpe

--- app/Models/Export.php
+++ app/Models/Export.php
@@ -9,4 +9,9 @@ class Export extends Model
     public function scopeLatest($query) {
         $query->orderBy('created_at','desc')->take(1);
     }
+
+    // An Export is complete when 'number_of_products' is not null
+    public function scopeCompleted($query) {
+        $query->whereNotNull('number_of_products');
+    }
 }
