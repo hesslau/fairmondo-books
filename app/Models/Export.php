22358e53f500493d69d905ba865aab375b11ff82
Henrik Hesslau <henrik@hesslau.org>
2017-12-04 16:40:22 +0700

    add status methods to export model

--- app/Models/Export.php
+++ app/Models/Export.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Export extends Model
 {
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'finished_at'
+    ];
+
     public function scopeLatest($query) {
         $query->orderBy('created_at','desc')->take(1);
     }
@@ -13,5 +19,25 @@ class Export extends Model
     // An Export is complete when 'number_of_products' is not null
     public function scopeCompleted($query) {
         $query->whereNotNull('number_of_products');
+    }
+
+    public function hasFinished() {
+        return ($this->finished_at != NULL);
+    }
+
+    public function isEmpty() {
+        return ($this->number_of_products === 0);
+    }
+
+    public function hasFailed() {
+        return ($this->hasFinished() && $this->number_of_products === NULL);
+    }
+
+    public function inProgress() {
+        return !$this->hasFinished();
+    }
+
+    public function getDuration() {
+        return $this->created_at->diffForHumans($this->finished_at, true);
     }
 }
