1a120a5293970e76daa039e8b13140a4909936c3
hesslau <henrik@hesslau.org>
2018-07-02 18:47:23 +0200

    fix namespace typo

--- app/FtpSettings.php
+++ app/FtpSettings.php
@@ -20,8 +20,8 @@ class FtpSettings {
     public function __construct(array $config=null) {
         if(is_null($config)) $config = config('ftp.updates');
 
-        if(!isset($config['host'])) throw new Exception("Host missing.");
-        if(!isset($config['user'])) throw new Exception("User missing.");
+        if(!isset($config['host'])) throw new \Exception("Host missing.");
+        if(!isset($config['user'])) throw new \Exception("User missing.");
         if(!isset($config['password'])) $config['password'] = '';
         if(!isset($config['directory'])) $config['directory'] = DIRECTORY_SEPARATOR;
         if(!isset($config['downloadDirectory'])) $config['downloadDirectory'] = storage_path('app/downloads');
